3ad0c176bb661abf513194abd1b1d253e6343ae9
unknown <prakashprm710@gmail.com>
2024-08-14 17:24:30 +0530

    inital commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "pizza1",
+    image: Pepperoni,
+    price: 1
+  },
+  {
+    name: "Pizza2",
+    image: Margherita,
+    price: 2,
+  },
+  {
+    name: "Pizza3",
+    image: PedroTechSpecial,
+    price: 3,
+  },
+  {
+    name: "Pizza4",
+    image: Vegan,
+    price: 4,
+  },
+  {
+    name: "Pizza5",
+    image: Pineapple,
+    price: 5,
+  },
+  {
+    name: "Pizza6",
+    image: Expensive,
+    price: 6,
+  },
+];
